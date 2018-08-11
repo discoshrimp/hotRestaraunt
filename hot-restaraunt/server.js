ac916afce6163797163b33b2986ae4c85a7408d6
discoshrimp <roydega@gmail.com>
2018-08-11 13:29:10 -0700

    new changes

--- hot-restaraunt/server.js
+++ hot-restaraunt/server.js
@@ -8,29 +8,17 @@ var PORT =5000
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-/*var tables = [
+var tables = [
 	{
-		inUse: false,
-		table:
-	}, {
-		inUse: false,
-		table:
-	}, {
-		inUse: false,
-		table:
-	}, {
-		inUse: false,
-		table:
-	}, {
-		inUse: false,
-		table:
-	}, {
-		inUse: false,
-		table:
+
+		routeName: "johnsmith",
+		name:"john smith",
+		phone:"444-444-4444",
+		email:"johnsmith@smith.com",
+		id: 00000,
 	}
-*/
-
-
+]
+var waitlist =[]
 
 
 app.get("/", function(req, res) {
@@ -58,5 +46,23 @@ app.get("/", function(req, res) {
 
   app.listen(PORT, function() {
 	console.log("App listening on PORT " + PORT);
-  });
+	});
+	
+
+	app.post("/api/tables", function(req, res) {
+		// req.body hosts is equal to the JSON post sent from the user
+		// This works because of our body-parser middleware
+		var newTable = req.body;
+	
+		// Using a RegEx Pattern to remove spaces from newTable
+		// You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+		newTable.routeName = newTable.name.replace(/\s+/g, "").toLowerCase();
+	
+		console.log(newTable);
+	
+		tables.push(newTable);
+	
+		res.json(newTable);
+	});
+	
   
